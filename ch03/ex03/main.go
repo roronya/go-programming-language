5a9cda3bdf0eb8d76539c8af0f7701454d2b797e
roronya <roronya628@gmail.com>
2021-10-09 21:06:49 +0900

    ex3.3, 3.4仮

---
+++ ch03/ex03/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320            // キャンバスの大きさ（画素数）
+	cells         = 100                 // 格子のます目の数
+	xyrange       = 30.0                // 軸の範囲(-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // x単位およびy単位あたりの画素数
+	zscale        = height * 0.4        // z単位あたりの画素数
+	angle         = math.Pi / 6         // x, y 軸の角度
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30度), cos(30度)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, z := corner(i+1, j)
+			bx, by, _ := corner(i, j)
+			cx, cy, _ := corner(i, j+1)
+			dx, dy, _ := corner(i+1, j+1)
+			if math.IsInf(ax, 0) || math.IsInf(ay, 0) || math.IsInf(bx, 0) || math.IsInf(by, 0) || math.IsInf(cx, 0) || math.IsInf(cy, 0) || math.IsInf(dx, 0) || math.IsInf(dy, 0) {
+				continue
+			}
+			fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g' fill='%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy, z)
+		}
+	}
+	fmt.Printf("</svg>")
+}
+
+func corner(i, j int) (float64, float64, float64) {
+	// ます目(i, j)のがどの点(x,y)を見つける
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// 面の高さzを計算する
+	z := f(x, y)
+
+	// (x,y,z)を2-D SVGキャンバス(sx, sy)へ等角的に投影
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, z
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	return math.Sin(r) / r
+}
