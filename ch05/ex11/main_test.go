e42dbfaff72f3ec3a2d1f1d8508b8701e7f6e352
roronya <roronya628@gmail.com>
2021-10-20 05:56:47 +0900

    add test

--- ch05/ex11/main_test.go
+++ ch05/ex11/main_test.go
@@ -27,3 +27,10 @@ func TestTopoSort_nonacyclic(t *testing.T) {
 		log.Fatalf("must not be detected asyclic.\ngot = %#v\n", actual)
 	}
 }
+
+func TestTopoSort_prereqs(t *testing.T) {
+	actual, err := topoSort(prereqs)
+	if err == nil {
+		log.Fatalf("must be detected asyclic.\ngot = %#v\n", actual)
+	}
+}
