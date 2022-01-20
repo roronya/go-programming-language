907042e37dbf2049c980020f5eabe285d72c890c
roronya <roronya628@gmail.com>
2022-01-21 08:40:21 +0900

    add ex11.3

---
+++ ch11/ex03/word_test.go
@@ -0,0 +1,48 @@
+package word
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25) // 24までのランダムな長さ
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
+
+func randomNotPalindrome(rng *rand.Rand) string {
+	return "a" + randomPalindrome(rng) + "b"
+}
+
+func TestRandomNotPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomNotPalindrome(rng)
+		if IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = true", p)
+		}
+	}
+}
