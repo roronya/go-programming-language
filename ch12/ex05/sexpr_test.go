383a2e511e611cad41e54cbb9daf8305c9789792
roronya <roronya628@gmail.com>
2022-01-30 21:53:43 +0900

    add ex12.5

---
+++ ch12/ex05/sexpr_test.go
@@ -0,0 +1,52 @@
+package sexpr
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestStrangelove(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	actual := Movie{}
+
+	b, err := Marshal(strangelove)
+	t.Logf("marshal: \n%s\n", b)
+	if err != nil {
+		t.Error(err)
+	}
+	err = json.Unmarshal(b, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+	if actual.Title != strangelove.Title {
+		t.Errorf("error")
+	}
+}
